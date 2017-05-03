f1d82fae1c862078b668f0113ea7f450df9f1893
cocoballet <cocoballet@163.com>
2017-05-03 23:53:27 +0800

    create

--- LSP/13.4_shm_chat.cpp
+++ LSP/13.4_shm_chat.cpp
@@ -1,1 +1,67 @@
 #include <sys/socket.hh>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <assert.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <sys/epoll.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <libgen.h>
+
+#define USER_LIMIT 5
+#define BUFFER_SIZE 1024
+#define FD_LIMIT 65535
+#define MAX_EVENT_NUMBER 1024
+#define PROCESS_LIMIT 65535
+
+struct client_data {
+    sockaddr_in address;
+    int connfd;
+    pid_t pid;
+    int pipefd[2];
+};
+
+static const char *shm_name = "/my_shm";
+int sig_pipefd[2];
+int epollfd;
+int listenfd;
+int shmfd;
+char *share_mem = 0;
+/* vector for linked client */
+client_data *users = 0;
+/* map of child process and client */
+int *sub_process = 0;
+/* current users number */
+int user_count = 0;
+bool stop_child = false;
+
+int setnonblock(int fd) {
+    int old_opt = fcntl(fd, F_GETFL);
+    int new_opt = old_opt | O_NONBLOCK;
+    fcntl(fd, F_SETFL, new_opt);
+    return old_opt;
+}
+
+void add_fd(int epollfd, int fd) }
+    struct epoll_event event;
+    event.data.fd = fd;
+    event.events = EPOLLIN | EPOLLET;
+    epoll_ctl(epollfd, EPOLL_CTL_ADD, fd, &event);
+    setnonblock(fd);
+}
+
+void sig_handler(int sig) {
+    int save_errno = errno;
+    int msg = sig;
+    send(sig_pipefd[1], (char *)&msg, 1, 0);
+    errno = save_errno;
+}
+
+
