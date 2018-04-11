72279a351b391071fcfed07ba50e158a0c5a0379
Andrea <cocoballet@163.com>
2018-04-11 16:29:36 +0800

    add

---
+++ LSP/15.6_thread_pool_web.cpp
@@ -0,0 +1,129 @@
+/*
+ * 15.6_thread_pool_web.cpp
+ * Author: Andrea
+ * Note: 
+ * Created Time: 2018年04月11日 星期三 13时53分17秒
+ */
+
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/epoll.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <assert.h>
+#include <errno.h>
+#include <string.h>
+#include <fcntl.h>
+
+#include "14.2_locker.h"
+#include "15.3_thread_pool.h"
+#include "15.4_http_conn.h"
+
+#define MAX_FD 65535
+#define MAX_EVENT_NUMBER 10000
+
+extern int addfd(int epollfd, int fd, bool one_shot);
+extern int removefd(int epollfd, int fd);
+
+void addsig(int sig, void(handler) (int), bool restart = true) {
+	struct sigaction sa;
+	memset(&sa, '\0', sizeof(sa));
+	sa.sa_handler = handler;
+	if(restart)
+		sa.sa_flags |= SA_RESTART;
+	sigfillset(&sa.sa_mask);
+	assert(sigaction(sig, &sa, NULL) != -1);
+}
+
+void show_error(int connfd, const char* info) {
+	printf("%s", info);
+	send(connfd, info, strlen(info), 0);
+	close(connfd);
+}
+
+int main(int argc, char* argv[]) {
+	if(argc <= 2) {
+		printf("usage: %s ip_address port_number\n", argv[0]);
+		return 1;
+	}
+	const char* ip = argv[1];
+	int port = atoi(argv[2]);
+
+	addsig(SIGPIPE, SIG_IGN);
+
+	ThreadPool<HTTPconn>* pool = NULL;
+	try {
+		pool = new ThreadPool<HTTPconn>;
+	}
+	catch(...) {
+		return 1;
+	}
+
+	HTTPconn* users = new HTTPconn[MAX_FD];
+	assert(users);
+	int user_count = 0;
+
+	int ret = 0;
+	struct sockaddr_in address;
+	bzero(&address, sizeof(address));
+	address.sin_family = AF_INET;
+	inet_pton(AF_INET, ip, &address.sin_addr);
+	address.sin_port = htons(port);
+
+	int listenfd = socket(PF_INET, SOCK_STREAM, 0);
+	assert(listenfd >= 0);
+	struct linger tmp = {1, 0};
+	setsockopt(listenfd, SOL_SOCKET, SO_LINGER, &tmp, sizeof(tmp));
+
+	ret = bind(listenfd, (struct sockaddr*) &address, sizeof(address));
+	assert(ret != -1);
+
+	struct epoll_event events[MAX_EVENT_NUMBER];
+	int epollfd = epoll_create(5);
+	assert(epollfd != -1);
+	addfd(epollfd, listenfd, false);
+	HTTPconn::m_epollfd = epollfd;
+
+	while(1) {
+		int number = epoll_wait(epollfd, events, MAX_EVENT_NUMBER, -1);
+		if((number < 0) && (errno != EINTR)) {
+			printf("epoll failure\n");
+			break;
+		}
+		for(int i = 0; i < number; i++) {
+			int sockfd = events[i].data.fd;
+			if(sockfd == listenfd) {
+				struct sockaddr_in client;
+				socklen_t client_len = sizeof(client);
+				int connfd = accept(listenfd, (struct sockaddr*) &client, &client_len);
+				if(connfd < 0) {
+					printf("errno: %d\n", errno);
+					continue;
+				}
+				if(HTTPconn::m_user_count >= MAX_FD) {
+					show_error(connfd, "Internal server busy");
+					continue;
+				}
+				users[connfd].init(connfd, client);
+			} else if(events[i].events & (EPOLLRDHUP | EPOLLHUP | EPOLLERR))
+				users[sockfd].close_conn();
+			else if(events[i].events & EPOLLIN) {
+				if(users[sockfd].read())
+					pool->append(users + sockfd);
+				else
+					users[sockfd].close_conn();
+			} else if(events[i].events & EPOLLOUT) {
+				if(!users[sockfd].write())
+					users[sockfd].close_conn();
+			} else { }
+		}
+	}
+	close(epollfd);
+	close(listenfd);
+	delete [] users;
+	delete pool;
+	return 0;
+}
